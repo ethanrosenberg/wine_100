daefa7164ff4696333b598e4d113b4b7414d7881
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-08 09:41:02 +0000

    Created basic menu structure

--- lib/wine_100/cli.rb
+++ lib/wine_100/cli.rb
@@ -1,5 +1,22 @@
 class Wine100::CLI
   def call
-    puts "Top 100 Wines"
+    puts "Welcome to the Top 100 Wines Rated by Wine Spectator"
+   
+    list_top_wines
+  end
+  
+  def list_top_wines
+    puts "1. 1998 Chateau Mouton Rothschild"
+    puts "2. 1988 Chateau Petrus Pomerol"
+  end
+  
+  def list_by_price_most_expensive
+    puts "Would you like to list the top 100 wines by price Highest - Lowest? (y/n)"
+  end
+  
+  def menu
+    puts "Please enter the number of your choice:"
+    puts "1. List of Top 100 Wines by prices Highest"
+    puts "2. List of Top 100 Wines by Rating"
   end
 end
