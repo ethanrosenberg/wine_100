47ec7a5975de528f0f47555ac4b6b406a002c205
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-10 04:54:59 +0000

    changed build wine title to class method

--- lib/wine.rb
+++ lib/wine.rb
@@ -16,13 +16,15 @@ class Wine100::Wine
   end
   
   def self.build_wine_from_table(row)
-    self.new()
+    self.new(build_wine_title(row))
   end
   
-  def build_wine_title(row)
+  def self.build_wine_title(row)
+    #binding.pry
     winery_main_title = row.css("td[class='name'] div[class='table-name'] span[class='wineName']").children[1].text.strip
     
-    winery_wine_title = winery.css("td[class='name'] div[class='table-name'] span[class='wineName'] span[class='sort-text']").text.strip
+    winery_wine_title = row.css("td[class='name'] div[class='table-name'] span[class='wineName']").children[0].text.strip
+    
     return "#{winery_main_title} #{winery_wine_title}"
   end
   
